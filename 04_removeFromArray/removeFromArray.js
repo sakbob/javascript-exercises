eeed577ca1b152be9171b99d9fdb956c11a0ffb7
Saksham <atomixdes@icloud.com>
2024-05-19 21:05:19 -0400

    Finish removeFromArray function.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const toReturn = [];
 
+    array.forEach((element) => {
+        if (!args.includes(element)) {
+            toReturn.push(element);
+        }
+    });
+
+    return toReturn;
 };
 
 // Do not edit below this line
