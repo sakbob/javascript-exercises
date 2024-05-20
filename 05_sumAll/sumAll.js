cfb50f4b310937c3c0085be1d0d4fa34a5544661
Saksham <atomixdes@icloud.com>
2024-05-19 21:14:20 -0400

    Finish sumAll function.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (min < 0 || max < 0) {
+        return "ERROR";
+    }
 
+    if (typeof(min) != "number" || typeof(max) != "number") {
+        return "ERROR";
+    }
+
+    let toReturn = 0;
+    if (min > max) {
+        toReturn = min;
+        min = max;
+        max = toReturn;
+        toReturn = 0;
+    }
+
+    for (let i = min; i <= max; i++) {
+        toReturn += i;
+    }
+
+    return toReturn;
 };
 
 // Do not edit below this line
