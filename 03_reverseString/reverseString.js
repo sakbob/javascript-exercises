d6eb1821634f48794a9a6e1d02423093ddcc6470
Saksham <atomixdes@icloud.com>
2024-05-19 20:38:31 -0400

    Finish reverseString function.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let toReturn = "";
 
+    for (let i = string.length - 1; i >= 0; i--) {
+        toReturn += string[i];
+    }
+
+    return toReturn;
 };
 
 // Do not edit below this line
