cfd6cad15cce4183002ebed429c0015ef577f768
Saksham <atomixdes@icloud.com>
2024-05-27 19:31:12 -0400

    Finish exercise.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let valid = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    let newString = string
+        .toLowerCase()
+        .split("")
+        .filter((char) => valid.includes(char))
+        .join("");
+
+    let reverseString = newString.split("").reverse().join("");
+
+    return reverseString === newString;
 };
 
 // Do not edit below this line
