3cb3a3ca0c45038d28260246227915f7c71def96
Saksham <atomixdes@icloud.com>
2024-05-27 20:13:31 -0400

    Finish exercise.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const sortedArray = array.sort(compare);
 
+    return sortedArray[0];
 };
+
+function compare(a, b) {
+    let aAge;
+    let bAge;
+
+    if (!("yearOfDeath" in a)) {
+        aAge = 2024 - a.yearOfBirth;
+    }
+    else {
+        aAge = a.yearOfDeath - a.yearOfBirth;
+    }
+
+    if (!("yearOfDeath" in b)) {
+        bAge = 2024 - b.yearOfBirth;
+    }
+    else {
+        bAge = b.yearOfDeath - b.yearOfBirth;
+    }
+
+    return bAge - aAge;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
